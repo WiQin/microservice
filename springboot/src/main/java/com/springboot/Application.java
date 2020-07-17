b8d9a04697164f139a79f2a7d925692dd058e38c
wangyanwen <wangyanwen@guanyisoft.com>
2020-07-17 17:24:21 +0800

    springboot&springcloud

---
+++ springboot/src/main/java/com/springboot/Application.java
@@ -0,0 +1,17 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ${Description}
+ *
+ * @author wyw
+ * @date 2020/07/17
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
---
+++ springboot/src/main/java/com/springboot/DemoController.java
@@ -0,0 +1,10 @@
+package com.springboot;
+
+/**
+ * ${Description}
+ *
+ * @author wyw
+ * @date 2020/07/17
+ */
+public class DemoController {
+}
